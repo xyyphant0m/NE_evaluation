d0137a0dd89e0dcdb9adcdc05e9b7e28b6784623
xyyphant0m <xyy18@mails.tsinghua.edu.cn>
2020-04-13 17:50:28 +0800

    first commit

--- evaluation/test.sh
+++ evaluation/test.sh
@@ -4,7 +4,6 @@ set -x
 
 python evaluation.py --datasets-path "../datasets/BlogCatalog/BlogCatalog.edgelist" \
                     --embeddings-path "../embeddings/BlogCatalog_alg1.emb" \
-                    "../embeddings/BlogCatalog_alg2.emb" \
                     --task "network_reconstruction" "node_classification" --seed 0 --C 1 --Np 1e6 --eval-metrics "precision_k" "AUC" \
                     --num-split 5 --start-train-ratio 10 --stop-train-ratio 90 --num-train-ratio 9 --num-workers 32
 
